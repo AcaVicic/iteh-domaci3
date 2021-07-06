8ea1fcf3c44a52f95d2e2aa3df07ae11aa4907f1
Aleksandar Vicic <acavicic@gmail.com>
2021-07-06 16:08:04 +0200

    Gotova pocetna stranica

--- src/components/Pocetna.js
+++ src/components/Pocetna.js
@@ -3,7 +3,14 @@ import "./Pocetna.scss"
 
 export default function Pocetna() {
     return (
-        <h1>Dobrodosli</h1> 
+        <div className="container">
+            <h1>Dobrodošli</h1> 
+            <h3>
+                Predstavljamo vam sajt koji će vam pomoci u odabiru gradova koje bi posetili. Gradovi u Srbiji su bogati raznim znamenitostima.
+                Za vise informacija pritisnite dugme ispod.
+            </h3>
+            <a href="/gradovi" className="btn btn-primary">Gradovi</a>
+        </div>
     )
 }
 
